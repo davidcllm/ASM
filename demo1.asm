de0c7b7a437f4bb8644d1c5acf03a315ddc5fe94
davidcllm <davcm24@hotmail.com>
2026-02-11 16:52:36 -0600

    Ejemplo 1

--- demo1.asm
+++ demo1.asm
@@ -1,7 +1,16 @@
 ; origen 100h, el codigo va a empezar en la localidad de memoria 100h (hexadecimal)
 org 100h 
 
+Jmp start ;satar al inicio del programa brinca a donde esta start:
 
+;DATA SEGMENT                          
+var1 DB 7       ;Guardar databyte (DB) con valor de 7 (var1=7) DB son 8 bits, 1 byte
+var2 DW 1234h   ;var2=1234 en heexadecimal dataw (DW) son 16 bits, 2 bytes
+
+
+start:      ;etiqueta, sirven para hacer brincos                       
+    mov al,var1     ;al=var1  al registro de 8 bits
+    mov bx,var2     ;bx=var2 bx registro de 16 bits                                                                                                          
 
 ret ; return, regresa el control al SO, es la ultima instruccion que se debe de tener
 
