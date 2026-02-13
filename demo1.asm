342eab11a87e4aebbefbf5bcf98329bedadb1ee3
davidcllm <davcm24@hotmail.com>
2026-02-13 16:58:31 -0600

    Ejemplo 7 [Hello World]

--- demo1.asm
+++ demo1.asm
@@ -1,38 +1,20 @@
-;PROGRAMA HELLO WORLD 1
+;PROGRAMA HELLO WORLD 2
+NAME "HI"
 
 ORG 100h
 
 JMP START    
 
 ;data string 
-MSG DB 'Hello, world!', 0    ;agregar el caracter nulo = 0
+MSG DB "Hello, world!", 0Dh, 0Ah, 24h    ;agregar el caracter nulo = 0
 
 START:
-    ;set the index register
-    MOV SI,0
-    
-NEXT_CHAR:
-    ;get current character
-    MOV AL, MSG[SI]    ;al = al contenid ode msg en la posicoin SI
-                          
-    ;is it zero?
-    ;if so stop printing                      
-    CMP AL,0
-    JE STOP
-                        
-    ;print character in teletype mode                    
-    MOV AH,0EH
-    INT 10h                          
-    
-    ;update index register by 1
-    INC SI                     
-    
-    ;go back to print another char
-    JMP NEXT_CHAR
-    
-STOP:
-    MOV AH,0 ;wait for any key press
-    INT 16h
+    ;obtener dir mem de msg
+    LEA DX, MSG            
+    MOV AH, 09h
+    INT 21h
+    MOV AH, 0
+    INT 16h    ;wait for any key
     
 RET   
     
