19a5cb9ebfed2e1d5864599274471126abda5d2a
davidcllm <davcm24@hotmail.com>
2026-02-13 16:23:15 -0600

    Ejemplo 5 [Escribir un caracter]

--- demo1.asm
+++ demo1.asm
@@ -1,23 +1,11 @@
-;PROGRAMA QUE MUESTRA EL FUNCIONAMIENTO 
-;DE LA INSTRUCCION SUMA Y RESTA
+;PROGRAMA QUE PARA ESCRIBIR UN CARACTER
 
-name "add-sub"
-ORG 100h 
+ORG 100h
 
-MOV AL,5
-MOV BL,10
+MOV AH,01h
+INT 21h     
 
-;5+10=15 DECIMAL
-ADD BL,AL   ;BL=BL+AL
-
-SUB BL,1    ;BL=BL-1
-
-;int 21h INTERRUPCION
-MOV AH,0 
-INT 16h ;press any key to continue
-
-
-RET ; return, regresa el control al SO, es la ultima instruccion que se debe de tener
+RET
 
 
 
