47495756af68faba4e7b53823a1d3803c1a94203
davidcllm <davcm24@hotmail.com>
2026-02-13 16:48:54 -0600

    Ejemplo 6 [Hello World]

--- demo1.asm
+++ demo1.asm
@@ -1,15 +1,42 @@
-;PROGRAMA PARA ESCRIBIR UN CARACTER A OUTPUT STANDARD 
+;PROGRAMA HELLO WORLD 1
 
 ORG 100h
 
-MOV DL,65    ;A=65 DECIMAL O 41 HEXADECIMAL
-MOV AH,02h
-INT 21h
+JMP START    
 
-RET
+;data string 
+MSG DB 'Hello, world!', 0    ;agregar el caracter nulo = 0
 
-
-
+START:
+    ;set the index register
+    MOV SI,0
+    
+NEXT_CHAR:
+    ;get current character
+    MOV AL, MSG[SI]    ;al = al contenid ode msg en la posicoin SI
+                          
+    ;is it zero?
+    ;if so stop printing                      
+    CMP AL,0
+    JE STOP
+                        
+    ;print character in teletype mode                    
+    MOV AH,0EH
+    INT 10h                          
+    
+    ;update index register by 1
+    INC SI                     
+    
+    ;go back to print another char
+    JMP NEXT_CHAR
+    
+STOP:
+    MOV AH,0 ;wait for any key press
+    INT 16h
+    
+RET   
+    
+    
 
 
 
