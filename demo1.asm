866f5fd7450e74d8aed35dcc20e5a37be04ac46d
davidcllm <davcm24@hotmail.com>
2026-02-16 17:06:18 -0600

    Ejercicio 9 [suma hex y despliegue decimal]

--- demo1.asm
+++ demo1.asm
@@ -1,52 +1,30 @@
-;PROGRAMA LEER INPUT DE USUARIO Y DESPLEGARLO
+;PROGRAMA SUMA HEXADECIMAL Y DESPLEGA EN DEECIMAL
 
 ORG 100h
 
-JMP START    
+MOV AH, 01h          ;leer un valor de la entrada estandar
+INT 21h
 
-;data string 
-MSG1 DB "Nombre: ", 24h             ;$
-MSG2 DB 0Dh, 0Ah, "Hola ", 24h      ;<ret> + new line + msg2 + $
-MSG3 DB " !!!", 0Dh, 0Ah, 24h       ;<ret> + new line + $
+MOV BL,AL           ;se guarda el valor leido al registro bl 
 
-VECNOM DB 80 DUP(?)                 ;guardar 80 loc memoria
+MOV AH, 01h         ;leer valor de entrada estandar
+INT 21h
 
-START:
-    LEA DX, MSG1              ;direccion de memoria 
-    MOV AH, 09h               ;imprime en pantalla
-    INT 21h
-    
-    MOV SI, 0                 ;contador de caracteres
-LEER:    
-    MOV AH, 01h               ;leer caracter de teclado y guardarlo en AL
-    INT 21h
-    
-    MOV VECNOM [SI], AL       ;se guarda el caracter en el string
-    INC SI                    ;incremento del contador para ell sig caracter
-    CMP AL, 0Dh               ;verificar que el caracter no se <ret>
-    JNE LEER                  ;no fue <enter>, leo el siguiente caracter
-                              ;el caracter enter termina la captura
-    DEC SI                    ;se refresa una posicion en el string
-    MOV VECNOM[SI], 24h       ;se coloca el $ al final del string
-    
-    LEA DX, MSG2              ;se eescribe Hola en pantalla
-    
-    MOV AH, 09h               ;imprime en pantalla
-    INT 21h
-                              
-    LEA DX, VECNOM            ;escribir nombre en pantalla
-                              
-    MOV AH, 09h               ;imprime en pantalla
-    INT 21h
-    
-    LEA DX, MSG3              ;escribir !!! en pantalla
-    
-    MOV AH, 09h               ;imprime en pantalla
-    INT 21h
-    
-    MOV AH, 0h                ; press any key...
-    INT 16h  
-    
+ADD AL,BL           ;suma valors leidos y se guarda en al
+                    ;al=al+bl
+                    
+MOV BL, 30h         ;desplazamiento de los caracteres numericos
+SUB AL,BL           ;al=al-bl
+
+MOV DL,AL           ;valor a desplegar en la salida estandar
+
+MOV AH, 02h         ;escribe el resultado en la salida estandar
+INT 21h
+
+MOV AH, 02h         ;escribe el resultado en la salida estandar
+
+MOV AH, 0
+INT 16h                 
     
     
 RET   
